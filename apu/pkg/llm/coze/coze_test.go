6210c9248cae18a078b0fa5dc5b480653988f332
favbox <978007000@qq.com>
2024-07-11 19:08:43 +0800

    init

--- apu/pkg/llm/coze/coze_test.go
+++ apu/pkg/llm/coze/coze_test.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 	"testing"
 
 	"apu/pkg/llm/coze"
@@ -15,19 +16,19 @@ import (
 )
 
 func TestStructureProject(t *testing.T) {
-	query := `项目名称 | 保利海晏天珺
-项目地址 | 中国 · 宁波
-完成时间 | 2024年
-设计面积｜185㎡、155 ㎡、145 ㎡ 
- 保利发展华东区域共享中心
-
-               浙江保利城市发展有限公司
-
-建筑设计 | 广州博瀚建筑设计事务所有限公司
-景观设计 | 加特林（重庆）景观规划设计有限公司
-室内设计 | MAUDEA 牧笛设计
-软装设计 | MAUDEA 牧笛设计
-项目摄影 | 鲁哈哈`
+	//	query := `项目名称 | 保利海晏天珺
+	//项目地址 | 中国 · 宁波
+	//完成时间 | 2024年
+	//设计面积｜185㎡、155 ㎡、145 ㎡
+	// 保利发展华东区域共享中心
+	//
+	//               浙江保利城市发展有限公司
+	//
+	//建筑设计 | 广州博瀚建筑设计事务所有限公司
+	//景观设计 | 加特林（重庆）景观规划设计有限公司
+	//室内设计 | MAUDEA 牧笛设计
+	//软装设计 | MAUDEA 牧笛设计
+	//项目摄影 | 鲁哈哈`
 	result, err := coze.StructureProject(query)
 	assert.Nil(t, err)
 	for _, msg := range result.Messages {
@@ -45,6 +46,7 @@ func TestStructureProject(t *testing.T) {
 		fmt.Println("时间", output.Time)
 		fmt.Println("名称", output.Name)
 		fmt.Println("标题", output.Title)
+		fmt.Println("关键词", output.Keyword)
 		fmt.Println("面积", output.Area)
 		fmt.Println("地点", output.Position)
 		fmt.Println("分类", output.Category)
@@ -53,7 +55,10 @@ func TestStructureProject(t *testing.T) {
 }
 
 //go:embed prompt.tpl
-var promptTpl string
+var prompt string
+
+//go:embed query.tpl
+var query string
 
 func TestOllamaAndQwen2(t *testing.T) {
 	ctx := context.Background()
@@ -65,39 +70,26 @@ func TestOllamaAndQwen2(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	query := `项目名称 | 保利海晏天珺
-项目地址 | 中国 · 宁波
-完成时间 | 2024年
-设计面积｜185㎡、155 ㎡、145 ㎡ 
- 保利发展华东区域共享中心
+	prompt := strings.ReplaceAll(prompt, "{{input}}", query)
 
-               浙江保利城市发展有限公司
+	//content := []llms.MessageContent{
+	//	llms.TextParts(llms.ChatMessageTypeSystem, prompt),
+	//	llms.TextParts(llms.ChatMessageTypeHuman, query),
+	//}
 
-建筑设计 | 广州博瀚建筑设计事务所有限公司
-景观设计 | 加特林（重庆）景观规划设计有限公司
-室内设计 | MAUDEA 牧笛设计
-软装设计 | MAUDEA 牧笛设计
-项目摄影 | 鲁哈哈`
+	////completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+	////	fmt.Println(string(chunk))
+	////	return nil
+	////}))
+	//completion, err := llm.GenerateContent(ctx, content)
 
-	//prompt := strings.ReplaceAll(promptTpl, "{{input}}", query)
-
-	content := []llms.MessageContent{
-		llms.TextParts(llms.ChatMessageTypeSystem, promptTpl),
-		llms.TextParts(llms.ChatMessageTypeHuman, query),
-	}
-
-	//completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-	//	fmt.Println(string(chunk))
-	//	return nil
-	//}))
-	completion, err := llm.GenerateContent(ctx, content)
-
-	//completion, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
+	completion, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, c := range completion.Choices {
-		fmt.Println(c.Content)
-	}
+	fmt.Println(completion)
+	//for _, c := range completion.Choices {
+	//	fmt.Println(c.Content)
+	//}
 	//_ = completion
 }
